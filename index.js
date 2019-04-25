f13e13e437025f7986929e9ddd2ce6956c10cf40
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-25 12:08:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  console.log('I was clicked!');
+});
 }
+
+addingEventListener()
